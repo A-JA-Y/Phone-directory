02bdac10980c48c2a3cd269b01594edf48600294
Ajay <mailtoajay7007@gmail.com>
2024-02-29 22:20:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import ReactDOM from "react-dom";
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  const [element, setElement] = useState(null);
+  function display() {
+    const newElement = React.createElement(
+      "div",
+      { id: "inputs" },
+      React.createElement("input", { id: "name", type: "text" ,placeholder: "NAME"}),
+      React.createElement("input", { id: "number", type: "number" , placeholder: "NUMBER"})
+    );
+
+    setElement(newElement);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="navbar">
+        <h1>Phone Directory</h1>
+      </div>
+
+      <button class='btn btn-default' onClick={display}>ADD</button>
+      <div id="root">{element}</div>
     </div>
   );
 }
